493534f3a4556df464e7e00c147f50775266b8bd
Park Gwonyong <rnjsdydqkr@naver.com>
2023-09-21 18:25:32 +0900

    first commit

---
+++ Calculator/Model/CalculatorBrain.swift
@@ -0,0 +1,20 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by 박권용 on 2023/09/21.
+//
+
+import Foundation
+
+class CalculatorBrain {
+  func setOperand(operand: Double) { }
+  
+  func performOperation(symbol: String) { }
+  
+  var result: Double {
+    get {
+      return 0.0
+    }
+  }
+}
---
+++ Calculator/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by 박권용 on 2022/02/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  @IBOutlet private var display: UILabel!
+  
+  private var userIsInTheMiddleOfTyping: Bool = false
+  
+  private var brain = CalculatorBrain()
+  
+  private var displayValue: Double {
+    get {
+      return Double(display.text!)!
+    }
+    set {
+      display.text = String(newValue)
+    }
+  }
+  
+  @IBAction private func touchDigit(_ sender: UIButton) {
+    let digit = sender.currentTitle!
+    if userIsInTheMiddleOfTyping {
+      let textCurrentlyInDisplay = display.text!
+      display.text = textCurrentlyInDisplay + digit
+    } else {
+      display.text = digit
+    }
+    userIsInTheMiddleOfTyping = true
+  }
+  
+  @IBAction private func performOperation(_ sender: UIButton) {
+    if userIsInTheMiddleOfTyping {
+      brain.setOperand(operand: displayValue)
+      userIsInTheMiddleOfTyping = false
+    }
+    if let mathematicalSymbol = sender.currentTitle {
+      brain.performOperation(symbol: mathematicalSymbol)
+    }
+    displayValue = brain.result
+  }
+  
+}
+
